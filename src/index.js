7d47b4b5f77e61477e19f4c03e79d5cea147918e
namanbekov <amanbekov.n@chocofood.kz>
2021-08-30 16:34:34 +0600

    Threads resolved

--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
 import "./assets/styles/global.scss";
-import { setActivePage } from './utils/utils'
+import * as utils from './utils/index'
 
-// Pages
+// Page wrappers
 const loginPageWrapper = document.querySelector(".login");
 const registrationPageWrapper = document.querySelector(".registration");
 const chatPageWrapper = document.querySelector(".chat-page");
 
 // Links
-const registrationBtn = document.querySelector(".registration-btn");
-const loginBtn = document.querySelector(".login-btn");
+const registrationBtn = document.querySelector(".btn__registration");
+const loginBtn = document.querySelector(".btn__login");
 const hasAccountLink = document.querySelector("#has-account-link");
 const noAccountLink = document.querySelector("#no-account-link");
 const logout = document.querySelector("#logout");
 
 loginBtn.addEventListener("click", () => {
-  setActivePage(chatPageWrapper);
+  utils.setActivePage(chatPageWrapper);
 });
 registrationBtn.addEventListener("click", () => {
-  setActivePage(chatPageWrapper);
+  utils.setActivePage(chatPageWrapper);
 });
 hasAccountLink.addEventListener("click", () => {
-  setActivePage(loginPageWrapper);
+  utils.setActivePage(loginPageWrapper);
 });
 noAccountLink.addEventListener("click", () => {
-  setActivePage(registrationPageWrapper);
+  utils.setActivePage(registrationPageWrapper);
 });
 logout.addEventListener('click', () => {
-  setActivePage(loginPageWrapper);
+  utils.setActivePage(loginPageWrapper);
 })
