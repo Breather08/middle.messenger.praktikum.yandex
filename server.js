84a217c1867626f933a3c33759a1098ff32ba4e5
namanbekov <amanbekov.n@chocofood.kz>
2021-08-31 21:54:39 +0600

    Init | Typescript & Eslint & Stylelint

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require("express");
-const path = require("path");
-const dotenv = require("dotenv");
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.listen(PORT, function () {
   console.log(`Listening server on port: ${PORT}`);
